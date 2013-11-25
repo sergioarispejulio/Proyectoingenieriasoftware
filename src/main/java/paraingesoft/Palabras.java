1be462881e7ce53e5f58fb64ba198c7578fec3cf
SergioArispeJulio <sergio_arispe@hotmail.com>
2013-11-24 23:09:22 -0400

    Usuarios casi listos

--- src/main/java/paraingesoft/Palabras.java
+++
@@ -1,8 +0,0 @@
-
-public class Palabras implements java.io.Serializable{
-
-	public transient int SSN;
-	public String palabra;
-	public int dificultad;//1-fácil:2-normal:3-difícil
-	public String categoria;
-}
